3f689144af9d360f25eafc9f052ecd544759caff
Ritesh Virendra Gupta <riteshg@Riteshs-MacBook-Powerhouse.local>
2025-04-08 13:34:33 +0530

    added television show details

--- js/script.js
+++ js/script.js
@@ -77,7 +77,82 @@ async function displayPopularShows() {
     document.querySelector('#popular-shows').appendChild(div);
   });
 }
+//Function to display show details
+async function displayShowDdetails() {
+  const series_id = window.location.search.split('?')[1];
+  const show = await fetchAPIData(`tv/${series_id}`);
 
+  //Overlay for background image
+  displayBackgroundImage('show', show.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <div class="details-top">
+          <div>
+          ${
+            show.poster_path
+              ? `
+            <img src = "https://image.tmdb.org/t/p/w500/${show.poster_path}"
+            class = "card-img-top"
+            alt = ${show.name}
+            />
+            `
+              : `
+            <img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />
+            `
+          }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)}
+            </p>
+            <p class="text-muted">Release Date: ${show.first_air_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${
+              show.homepage
+            }" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span> ${
+              show.number_of_episodes
+            }</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air:</span> ${
+                show.last_air_date
+              }
+            </li>
+            <li><span class="text-secondary">Status:</span> Released</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+          ${show.production_companies
+            .map(
+              (company) =>
+                `<span>
+                ${company.name}
+            </span>`
+            )
+            .join(', ')}
+          </div>
+        </div>
+  `;
+  document.querySelector('#show-details').appendChild(div);
+}
 //Function to display movie details
 async function displayMovieDetails() {
   const movieId = window.location.search.split('?')[1];
@@ -242,7 +317,7 @@ function init() {
       displayMovieDetails();
       break;
     case '/tv-details.html':
-      console.log('TV Details');
+      displayShowDdetails();
       break;
     case '/search.html':
       console.log('Search');
